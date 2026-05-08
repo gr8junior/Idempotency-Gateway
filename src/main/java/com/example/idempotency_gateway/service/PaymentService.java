f4a72e9e6696e860d9fbe99ee4eddcd1003deed9
Sampson Aidoo <samqwame10@gmail.com>
2026-05-08 14:12:20 +0000

    Implement payment processing with delay

--- src/main/java/com/example/idempotency_gateway/service/PaymentService.java
+++ src/main/java/com/example/idempotency_gateway/service/PaymentService.java
@@ -1,5 +1,24 @@
 package com.example.idempotency_gateway.service;
+import org.springframework.stereotype.Service;
 
+import com.example.idempotency_gateway.model.PaymentRequest;  
+
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
+
+
+@Service
 public class PaymentService {
-    
+     public Map<String, Object> process(PaymentRequest request) throws InterruptedException {
+        Thread.sleep(2000); // simulate delay
+
+        Map<String, Object> result = new HashMap<>();
+        result.put("message", "Charged " + request.getAmount() + " " + request.getCurrency());
+        result.put("amount", request.getAmount());
+        result.put("currency", request.getCurrency());
+        result.put("processedAt", Instant.now().toString());
+
+        return result;
+    }
 }
