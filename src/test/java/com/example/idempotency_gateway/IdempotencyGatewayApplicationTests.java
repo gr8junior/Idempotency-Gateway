6aaddbf5bf8d82ed8bbc8bc3c20b820dad1ce3ee
Sampson Aidoo <samqwame10@gmail.com>
2026-05-08 00:48:51 +0000

    initialize Spring boot project with Maven setup

---
+++ src/test/java/com/example/idempotency_gateway/IdempotencyGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.idempotency_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdempotencyGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
