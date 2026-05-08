ecd24aa99dd062e74a7dea17058e63fcbaf078ee
Sampson Aidoo <samqwame10@gmail.com>
2026-05-08 14:34:29 +0000

    feat: request hashing

--- src/main/java/com/example/idempotency_gateway/utils/HashUtil.java
+++ src/main/java/com/example/idempotency_gateway/utils/HashUtil.java
@@ -1,5 +1,25 @@
 package com.example.idempotency_gateway.utils;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.security.MessageDigest;
+
 public class HashUtil {
-    
+     private static final ObjectMapper mapper = new ObjectMapper();
+
+    public static String hash(Object body) {
+        try {
+            String json = mapper.writeValueAsString(body);
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] hashBytes = digest.digest(json.getBytes());
+
+            StringBuilder hex = new StringBuilder();
+            for (byte b : hashBytes) {
+                hex.append(String.format("%02x", b));
+            }
+            return hex.toString();
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
