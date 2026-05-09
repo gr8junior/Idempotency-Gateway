8705145b46f0026648cb73ca49e399e88f7af581
Sampson Aidoo <samqwame10@gmail.com>
2026-05-09 00:14:39 +0000

    feat: add response model for payment processing

--- src/main/java/com/example/idempotency_gateway/model/StoredResponse.java
+++ src/main/java/com/example/idempotency_gateway/model/StoredResponse.java
@@ -1,5 +1,14 @@
 package com.example.idempotency_gateway.model;
 
 public class StoredResponse {
-    
+      private int statusCode;
+    private Object body;
+
+    public StoredResponse(int statusCode, Object body) {
+        this.statusCode = statusCode;
+        this.body = body;
+    }
+
+    public int getStatusCode() { return statusCode; }
+    public Object getBody() { return body; }
 }
