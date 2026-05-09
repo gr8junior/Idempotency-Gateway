0ad9949ed2bee8018d8bfc2c7a2de18a0078d40a
Sampson Aidoo <samqwame10@gmail.com>
2026-05-09 00:13:32 +0000

    feat: add request model for payment processing

--- src/main/java/com/example/idempotency_gateway/model/PaymentRequest.java
+++ src/main/java/com/example/idempotency_gateway/model/PaymentRequest.java
@@ -1,5 +1,12 @@
 package com.example.idempotency_gateway.model;
 
 public class PaymentRequest {
-    
+     private double amount;
+    private String currency;
+
+    public double getAmount() { return amount; }
+    public void setAmount(double amount) { this.amount = amount; }
+
+    public String getCurrency() { return currency; }
+    public void setCurrency(String currency) { this.currency = currency; }
 }
